9a100655b666da0346eae597e4d7dd3428741108
Mohamed El Amine Kherroubi <elaminekherroubi@gmail.com>
2025-12-11 00:33:30 +0100

    Some config modifications

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,10 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "*.mdx", base: "./src/content/blog" }),
+  loader: glob({
+    pattern: "**/*.mdx",
+    base: "./src/content/blog",
+  }),
   schema: z.object({
     title: z.string(),
     excerpt: z.string(),
@@ -13,7 +16,10 @@ const blog = defineCollection({
 });
 
 const portfolio = defineCollection({
-  loader: glob({ pattern: "*.mdx", base: "./src/content/portfolio" }),
+  loader: glob({
+    pattern: "**/*.mdx",
+    base: "./src/content/portfolio",
+  }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
